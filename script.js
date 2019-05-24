b2355c633de3113585e65e5ff5567ad59e9b48e3
J4nis05 <50984458+J4nis05@users.noreply.github.com>
2019-05-24 14:34:49 +0200

    new file js
    some text

---
+++ script.js
@@ -0,0 +1,38 @@
+function myFunction() {
+    var x = document.getElementById("n1").value;
+    var y = document.getElementById("n2").value;
+    var operation = document.getElementById("op").value;
+    var res = 0;
+    //console.log("Halloooo", operation);
+
+    if (operation == "Plus") {
+        res = Number(x) + Number(y);
+        document.getElementById("demo").innerHTML = res;
+        console.log(" + ");
+    }
+
+    else if (operation == "Minus") {
+        res = Number(x) - Number(y);
+        document.getElementById("demo").innerHTML = res;
+        console.log(" - ")
+    }
+
+    else if (operation == "Mal") {
+        res = Number(x) * Number(y);
+        document.getElementById("demo").innerHTML = res;
+        console.log(" * ")
+    }
+
+    else if (operation == "Durch") {
+        res = Number(x) / Number(y);
+        document.getElementById("demo").innerHTML = res;
+        console.log(" : ")
+    }
+}
+
+function myFunction2() {
+    if (new Date().getHours() < 18) {
+        document.getElementById("demo").innerHTML = "Good day!";
+        console.log("Date:", new Date());
+    }
+}
